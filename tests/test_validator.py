0ae882a18b256e4b0a62a54c99555558f2dd34d4
Haoran Zhang <hoyincheung44@gmail.com>
2021-04-24 17:44:08 +0000

    add special character test function

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -47,3 +47,13 @@ class TestValidator(unittest.TestCase):
 
         # Assert
         self.assertTrue(result)
+
+    def test_it_will_reject_username_if_there_is_any_special_characters(self):
+        # Assume
+        username = '@Reval'
+
+        # Action
+        result = self.validator.username_is_valid(username)
+
+        # Assert
+        self.assertFalse(result)
